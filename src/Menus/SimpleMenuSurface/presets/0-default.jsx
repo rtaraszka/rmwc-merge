ff64ab88de6d87320891f6e0d548c9b277997c22
Earl Debicki <bartosz.debicki@uxpin.com>
2019-03-26 16:02:19 +0100

    update presets by new jsx syntax

--- src/Menus/SimpleMenuSurface/presets/0-default.jsx
+++ src/Menus/SimpleMenuSurface/presets/0-default.jsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import SimpleMenuSurface from "../SimpleMenuSurface";
+import Button from "../../../Button/Button";
 
 export default (
-  <SimpleMenuSurface uxpId="0" handle={`<Button raised>Open Simple Menu Surface</Button>`}>
+  <SimpleMenuSurface
+    uxpId="0"
+    handle={<Button raised uxpId="0.1">Open Simple Menu Surface</Button>}
+  >
     <div uxpId="1" style={{padding: '1rem', width: '8rem'}}>Make the content whatever you want.</div>
   </SimpleMenuSurface>
 );
